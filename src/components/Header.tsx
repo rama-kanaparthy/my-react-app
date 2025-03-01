aeeaa014375a94f03ca4fa652bac61a3e51da105
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-02 00:08:09 +0530

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
 function Header() {
     return (
-      <header style={{ backgroundColor: "#333", padding: "15px", color: "white", textAlign: "center" }}>
+      <header style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        background: "#333",
+        color: "white",
+        padding: "10px 0",
+        textAlign: "center",
+        zIndex: 1000, // Ensures it's above other content
+      }}>
         <h1>My React App</h1>
       </header>
     );
