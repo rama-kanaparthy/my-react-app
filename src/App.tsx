c7f40c3641b76d18d0838b9b3f12fbb4ba2b9a6e
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-03-01 23:15:37 +0530

    Modify App.tsx and see live updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to My First React App</h1>
+        <p>Learning React with Vite is fast and fun! 🚀</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
